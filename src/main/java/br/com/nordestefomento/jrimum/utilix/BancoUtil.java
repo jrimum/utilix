89ceb67813f8e35488c4090be13abb025c60d5ee
Gilmar P.S.L <gilmatryx@gmail.com>
2010-02-17 21:20:18 +0000

    [jri:milestone:0.2-Helio] [jri:ticket:78]  * Revisão também do conteúdo informativo do javadoc. {{{ Métodos  * @since                    [OK]  * Tags html                 [OK] Classe  * Tags html                 [OK]  * Descrição simples         [OK]  * @since                    [OK]  * @version                  [OK] Arquivo  * Licença apache            [OK] }}}

--- src/main/java/br/com/nordestefomento/jrimum/utilix/BancoUtil.java
+++ src/main/java/br/com/nordestefomento/jrimum/utilix/BancoUtil.java
@@ -38,12 +38,10 @@ import java.util.GregorianCalendar;
 
 import org.apache.commons.lang.time.DateUtils;
 
-import br.com.nordestefomento.jrimum.JRimumException;
-
 /**
  * <p>
  * Serviços utilitários do universo bancário, como por exemplo calcular o fator
- * de vencimento.</code>
+ * de vencimento de boletos.</code>
  * </p>
  * 
  * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L</a>
@@ -65,87 +63,98 @@ public class BancoUtil implements Serializable {
 
 	/**
 	 * <p>
-	 * Data base para o cálculo do fator de vencimento fixada em 07.10.1997
-	 * (03.07.2000 retrocedidos 1000 dias do início do processo) segundo a
+	 * Data base para o cálculo do fator de vencimento fixada em 07/10/1997 pela
 	 * FEBRABAN.
 	 * </p>
 	 */
 	private static final Date DATA_BASE_DO_FATOR_DE_VENCIMENTO = new GregorianCalendar(
 			1997, Calendar.OCTOBER, 7).getTime();
 
+	/**
+	 *<p>
+	 * Data máxima alcançada pelo fator de vencimento com base fixada em
+	 * 07/10/1997.
+	 * </p>
+	 */
 	private static final Date DATA_LIMITE_DO_FATOR_DE_VENCIMENTO = new GregorianCalendar(
 			2025, Calendar.FEBRUARY, 21).getTime();
 
 	/**
+	 * <p>
+	 * Calcula o fator de vencimento a partir da subtração entre a DATA DE
+	 * VENCIMENTO de um título e a DATA BASE fixada em 07/10/1997.
+	 * </p>
 	 * 
-	 *<p>
-	 * Calcula o fator de vencimento, com base na subtração entre a DATA DE
-	 * VENCIMENTO do título e a DATA BASE , fixada em 07.10.1997 (03.07.2000
-	 * retrocedidos 1000 dias do início do processo). Trata-se de um referencial
-	 * numérico de 4 dígitos, situado nas quatro primeiras posições do campo
-	 * “valor”, que representa a quantidade de dias decorridos da data base à
-	 * data de vencimento do título.
-	 *</p>
-	 *<p>
-	 * Os bloquetos de cobrança emitidos a partir de 1º de setembro de 2000
-	 * (primeiro dia útil = 03/07/2000 - SEGUNDA) OBRIGATORIAMENTE devem conter
-	 * essas características, para que quando forem capturados pela rede
-	 * bancária, os sistemas possam realizar operação inversa, ou seja,
-	 * adicionar à data base o fator de vencimento capturado, obtendo, dessa
-	 * forma, a data do vencimento do bloqueto.<br/>
+	 * <p>
+	 * O fator de vencimento nada mais é que um referencial numérico de 4
+	 * dígitos que representa a quantidade de dias decorridos desde a data base
+	 * (07/10/1997) até a data de vencimento do título. Ou seja, a diferença em
+	 * dias entre duas datas.
+	 * </p>
 	 * 
-	 * Exemplos: 03/07/2000(Fator = 1000), 05/07/2000(Fator = 1002), 01/05/2002
-	 * (Fator = 1667), 05/07/2000(Fator = 1002) e 21/02/2025(Fator = 9999).
-	 *</p>
+	 * <p>
+	 * Exemplos:
+	 * </p>
+	 * <ul type="circule"> <li>07/10/1997 (Fator = 0);</li> <li>03/07/2000
+	 * (Fator = 1000);</li> <li>05/07/2000 (Fator = 1002);</li> <li>01/05/2002
+	 * (Fator = 1667);</li> <li>21/02/2025 (Fator = 9999).</li> </ul>
 	 * 
-	 *<p>
-	 * Observações:
-	 * <ul>
-	 * <li>
-	 * A data base para o cálculo do fator de vencimento é 07/10/1997 (Fator de
-	 * vencimento = 0).Caso a data de vencimento seja anterior a data base, uma
-	 * exceção do tipo IllegalArgumentException será lançada.</li>
-	 * <li>
-	 * A data limite para o cálculo do fator de vencimento é 21/02/2025 (Fator
-	 * de vencimento = 9999) Caso a data de vencimento seja posterior a data
-	 * limite, uma exceção do tipo IllegalArgumentException será lançada.</li>
-	 * </ul>
-	 *</p>
+	 * <p>
+	 * Funcionamento:
+	 * </p>
+	 * 
+	 * <ul type="square"> <li>Caso a data de vencimento seja anterior a data
+	 * base (Teoricamente fator negativo), uma exceção do tipo
+	 * IllegalArgumentException será lançada.</li> <li>A data limite para o
+	 * cálculo do fator de vencimento é 21/02/2025 (Fator de vencimento = 9999).
+	 * Caso a data de vencimento seja posterior a data limite, uma exceção do
+	 * tipo IllegalArgumentException será lançada.</li> </ul>
+	 * 
+	 * <p>
+	 * <strong>ATENÇÃO</strong>, esse cálculo se refere a títulos em cobrança,
+	 * ou melhor: BOLETOS. Desta forma, lembramos que a DATA BASE é uma norma da
+	 * FEBRABAN. Essa norma diz que todos os boletos emitidos a partir de 1º de
+	 * setembro de 2000 (primeiro dia útil = 03/07/2000 - SEGUNDA) devem seguir
+	 * esta regra de cálculo para compor a informação de vencimento no código de
+	 * barras. Portanto, boletos no padrão FEBRABAN quando capturados por
+	 * sistemas da rede bancária permitem que se possa realizar a operação
+	 * inversa, ou seja, adicionar à data base o fator de vencimento capturado.
+	 * Obtendo então a data de vencimento deste boleto.
+	 * </p>
 	 * 
 	 * @param dataVencimento
-	 *            - data de vencimento.
-	 * @return fator de vencimento.
+	 *            data de vencimento de um título
+	 * @return fator de vencimento calculado
+	 * @throws IllegalArgumentException
 	 * 
-	 * @throws JRimumException
-	 *             Caso alguma condição de entrada não seja respeitada.
 	 * @since 0.2
 	 */
+
 	public static int calculceFatorDeVencimento(Date dataVencimento)
-			throws JRimumException {
+			throws IllegalArgumentException {
 
 		Date dataVencTruncada = null;
 		int fator;
 
 		if (isNull(dataVencimento)) {
-			throw new JRimumException(new IllegalArgumentException(
+			throw new IllegalArgumentException(
 					"Impossível realizar o cálculo do fator"
-							+ " de vencimento de uma data nula."));
+							+ " de vencimento de uma data nula.");
 		} else {
 			dataVencTruncada = DateUtils
 					.truncate(dataVencimento, Calendar.DATE);
 			if (dataVencTruncada.before(DATA_BASE_DO_FATOR_DE_VENCIMENTO)
 					|| dataVencTruncada
 							.after(DATA_LIMITE_DO_FATOR_DE_VENCIMENTO)) {
-				throw new JRimumException(
-						new IllegalArgumentException(
-								"Para o cálculo do fator de"
-										+ " vencimento se faz necessário informar uma data entre"
-										+ " "
-										+ DateUtil.FORMAT_DD_MM_YYYY
-												.format(DATA_BASE_DO_FATOR_DE_VENCIMENTO)
-										+ " e "
-										+ DateUtil.FORMAT_DD_MM_YYYY
-												.format(DATA_LIMITE_DO_FATOR_DE_VENCIMENTO)));
+				throw new IllegalArgumentException(
+						"Para o cálculo do fator de"
+								+ " vencimento se faz necessário informar uma data entre"
+								+ " "
+								+ DateUtil.FORMAT_DD_MM_YYYY
+										.format(DATA_BASE_DO_FATOR_DE_VENCIMENTO)
+								+ " e "
+								+ DateUtil.FORMAT_DD_MM_YYYY
+										.format(DATA_LIMITE_DO_FATOR_DE_VENCIMENTO));
 			} else {
 				fator = (int) DateUtil.calculeDiferencaEmDias(
 						DATA_BASE_DO_FATOR_DE_VENCIMENTO, dataVencTruncada);
