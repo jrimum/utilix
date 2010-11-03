4cdc26c2a10bc62971065ee8a9789a6bf723491a
Gilmar P.S.L <gilmatryx@gmail.com>
2010-11-03 10:21:17 +0000

    '''REVISÃO:'''
     * De NPE para IAE

--- src/main/java/org/jrimum/utilix/Collections.java
+++ src/main/java/org/jrimum/utilix/Collections.java
@@ -38,8 +38,8 @@ import java.util.Map;
  * </p>
  * 
  * <p>
- * Fornece métodos booleanos e métodos que verificam se uma coleção está de acordo
- * com o desejado e, caso não estejam, lançam exceção.
+ * Fornece métodos booleanos e métodos que verificam se uma coleção está de
+ * acordo com o desejado e, caso não estejam, lançam exceção.
  * </p>
  * 
  * @author <a href="http://gilmatryx.googlepages.com/">Gilmar P.S.L.</a>
@@ -50,26 +50,30 @@ import java.util.Map;
  * @version 0.2
  */
 public final class Collections {
-	
+
 	/**
 	 * Utility class pattern: classe não instanciável
 	 * 
-	 * @throws AssertionError Caso haja alguma tentativa de utilização deste construtor.
+	 * @throws AssertionError
+	 *             Caso haja alguma tentativa de utilização deste construtor.
 	 */
 	private Collections() {
-		
+
 		throw new AssertionError("NOT SUPPORTED OPERATION!");
 	}
 
 	/**
 	 * <p>
-	 * Verifica se a <code>Collection</code> passada por parâmetro é <code>null</code> ou 
-	 * <strong>não</strong> possui elementos e lança exceção caso não preencha estes requisitos.
+	 * Verifica se a <code>Collection</code> passada por parâmetro é
+	 * <code>null</code> ou <strong>não</strong> possui elementos e lança
+	 * exceção caso não preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param collection - Instância de <code>Collection</code> analisada.
+	 * @param collection
+	 *            - Instância de <code>Collection</code> analisada.
 	 * 
-	 * @thows IllegalArgumentException - Caso a coleção <strong>não</strong> seja <code>null</code> e possua elementos.
+	 * @thows IllegalArgumentException - Caso a coleção <strong>não</strong>
+	 *        seja <code>null</code> e possua elementos.
 	 * 
 	 * @see #checkEmpty(Collection, String)
 	 * @see #isEmpty(Collection)
@@ -78,21 +82,25 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkEmpty(Collection<?> collection) {
-		
-		checkEmpty(collection, "Collection não nulo e com elementos! Valor [" + collection + "].");
+
+		checkEmpty(collection, "Collection não nulo e com elementos! Valor ["
+				+ collection + "].");
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se a <code>Collection</code> passado por parâmetro é <code>null</code> ou 
-	 * <strong>não</strong> possui elementos e lança exceção, com a mensagem informada, 
-	 * caso não preencha estes requisitos.
+	 * Verifica se a <code>Collection</code> passado por parâmetro é
+	 * <code>null</code> ou <strong>não</strong> possui elementos e lança
+	 * exceção, com a mensagem informada, caso não preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param collection - Instância de <code>Collection</code> analisada.
-	 * @param message - Mensagem utilizada na exceção.
+	 * @param collection
+	 *            - Instância de <code>Collection</code> analisada.
+	 * @param message
+	 *            - Mensagem utilizada na exceção.
 	 * 
-	 * @thows IllegalArgumentException - Caso a coleção <strong>não</strong> seja <code>null</code> e possua elementos.
+	 * @thows IllegalArgumentException - Caso a coleção <strong>não</strong>
+	 *        seja <code>null</code> e possua elementos.
 	 * 
 	 * @see #isEmpty(Collection)
 	 * @see #isNotEmpty(Collection)
@@ -100,23 +108,26 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkEmpty(Collection<?> collection, String message) {
-		
-		if(hasElement(collection)){
-			
+
+		if (hasElement(collection)) {
+
 			throw new IllegalArgumentException(message);
 		}
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se a <code>Collection</code> passada por parâmetro <strong>não</strong> é <code>null</code> e 
-	 * possui elementos e lança exceção caso não preencha estes requisitos.
+	 * Verifica se a <code>Collection</code> passada por parâmetro
+	 * <strong>não</strong> é <code>null</code> e possui elementos e lança
+	 * exceção caso não preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Collection</code> analisada.
+	 * @param map
+	 *            - Instância de <code>Collection</code> analisada.
 	 * 
-	 * @throws NullPointerException - Caso a coleção seja <code>null</code>.
-	 * @thows IllegalArgumentException - Caso a coleção <strong>não</strong> possua elementos.
+	 * @throws IllegalArgumentException
+	 *             - Caso a coleção seja <code>null</code> ou a coleção
+	 *             <strong>não</strong> possua elementos.
 	 * 
 	 * @see #checkNotEmpty(Collection, String)
 	 * @see #isEmpty(Collection)
@@ -125,21 +136,25 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkNotEmpty(Collection<?> collection) {
-		
+
 		checkNotEmpty(collection, "Objeto nulo!", "Collection sem elementos!");
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se a <code>Collection</code> passada por parâmetro <strong>não</strong> é <code>null</code> e 
-	 * possui elementos e lança exceção, com a mensagem informada, caso não preencha estes requisitos.
+	 * Verifica se a <code>Collection</code> passada por parâmetro
+	 * <strong>não</strong> é <code>null</code> e possui elementos e lança
+	 * exceção, com a mensagem informada, caso não preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param collection - Instância de <code>Collection</code> analisada.
-	 * @param message - Mensagem utiliada na exceção.
+	 * @param collection
+	 *            - Instância de <code>Collection</code> analisada.
+	 * @param message
+	 *            - Mensagem utiliada na exceção.
 	 * 
-	 * @throws NullPointerException - Caso a coleção seja <code>null</code>.
-	 * @thows IllegalArgumentException - Caso a coleção <strong>não</strong> possua elementos.
+	 * @throws IllegalArgumentException
+	 *             - Caso a coleção seja <code>null</code> ou a coleção
+	 *             <strong>não</strong> possua elementos.
 	 * 
 	 * @see #isEmpty(Collection)
 	 * @see #isNotEmpty(Collection)
@@ -147,19 +162,22 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkNotEmpty(Collection<?> collection, String message) {
-		
+
 		checkNotEmpty(collection, message, message);
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o <code>Map</code> passado por parâmetro é <code>null</code> ou 
-	 * <strong>não</strong> possui elementos e lança exceção caso não preencha estes requisitos.
+	 * Verifica se o <code>Map</code> passado por parâmetro é <code>null</code>
+	 * ou <strong>não</strong> possui elementos e lança exceção caso não
+	 * preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Map</code> analisada.
+	 * @param map
+	 *            - Instância de <code>Map</code> analisada.
 	 * 
-	 * @thows IllegalArgumentException - Caso o mapa <strong>não</strong> seja <code>null</code> e possua elementos.
+	 * @thows IllegalArgumentException - Caso o mapa <strong>não</strong> seja
+	 *        <code>null</code> e possua elementos.
 	 * 
 	 * @see #checkEmpty(Map, String)
 	 * @see #isEmpty(Map)
@@ -168,21 +186,24 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkEmpty(Map<?, ?> map) {
-		
+
 		checkEmpty(map, "Map não nulo e com elementos. Valor [" + map + "]");
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o <code>Map</code> passado por parâmetro é <code>null</code> ou 
-	 * <strong>não</strong> possui elementos e lança exceção, com a mensagem informada, 
-	 * caso não preencha estes requisitos.
+	 * Verifica se o <code>Map</code> passado por parâmetro é <code>null</code>
+	 * ou <strong>não</strong> possui elementos e lança exceção, com a mensagem
+	 * informada, caso não preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Map</code> analisada.
-	 * @param message - Mensagem utilizada na exceção.
+	 * @param map
+	 *            - Instância de <code>Map</code> analisada.
+	 * @param message
+	 *            - Mensagem utilizada na exceção.
 	 * 
-	 * @thows IllegalArgumentException - Caso o mapa <strong>não</strong> seja <code>null</code> e possua elementos.
+	 * @thows IllegalArgumentException - Caso o mapa <strong>não</strong> seja
+	 *        <code>null</code> e possua elementos.
 	 * 
 	 * @see #isEmpty(Map)
 	 * @see #isNotEmpty(Map)
@@ -190,23 +211,26 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkEmpty(Map<?, ?> map, String message) {
-		
-		if(hasElement(map)){
-			
+
+		if (hasElement(map)) {
+
 			throw new IllegalArgumentException(message);
 		}
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o <code>Map</code> passado por parâmetro <strong>não</strong> é <code>null</code> e 
-	 * possui elementos e lança exceção caso não preencha estes requisitos.
+	 * Verifica se o <code>Map</code> passado por parâmetro <strong>não</strong>
+	 * é <code>null</code> e possui elementos e lança exceção caso não preencha
+	 * estes requisitos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Map</code> analisada.
+	 * @param map
+	 *            - Instância de <code>Map</code> analisada.
 	 * 
-	 * @throws NullPointerException - Caso o mapa seja <code>null</code>.
-	 * @thows IllegalArgumentException - Caso o mapa <strong>não</strong> possua elementos.
+	 * @throws IllegalArgumentException
+	 *             - Caso o mapa seja <code>null</code> ou o mapa
+	 *             <strong>não</strong> possua elementos.
 	 * 
 	 * @see #checkNotEmpty(Map, String)
 	 * @see #isEmpty(Map)
@@ -215,21 +239,25 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkNotEmpty(Map<?, ?> map) {
-		
+
 		checkNotEmpty(map, "Objeto nulo", "Map sem elementos");
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o <code>Map</code> passado por parâmetro <strong>não</strong> é <code>null</code> e 
-	 * possui elementos e lança exceção, com a mensagem informada, caso não preencha estes requisitos.
+	 * Verifica se o <code>Map</code> passado por parâmetro <strong>não</strong>
+	 * é <code>null</code> e possui elementos e lança exceção, com a mensagem
+	 * informada, caso não preencha estes requisitos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Map</code> analisada.
-	 * @param message - Mensagem utiliada na exceção.
+	 * @param map
+	 *            - Instância de <code>Map</code> analisada.
+	 * @param message
+	 *            - Mensagem utiliada na exceção.
 	 * 
-	 * @throws NullPointerException - Caso o mapa seja <code>null</code>.
-	 * @thows IllegalArgumentException - Caso o mapa <strong>não</strong> possua elementos.
+	 * @throws IllegalArgumentException
+	 *             - Caso o mapa seja <code>null</code> ou o mapa
+	 *             <strong>não</strong> possua elementos.
 	 * 
 	 * @see #isEmpty(Map)
 	 * @see #isNotEmpty(Map)
@@ -237,33 +265,35 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static void checkNotEmpty(Map<?, ?> map, String message) {
-		
+
 		checkNotEmpty(map, message, message);
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se a <code>Collection</code> passada por parâmetro é <code>null</code> ou 
-	 * <strong>não</strong> possui elementos. 
+	 * Verifica se a <code>Collection</code> passada por parâmetro é
+	 * <code>null</code> ou <strong>não</strong> possui elementos.
 	 * </p>
 	 * 
-	 * @param collection - Instância de <code>Collection</code> analisada.
+	 * @param collection
+	 *            - Instância de <code>Collection</code> analisada.
 	 * @return <code>!hasElement(collection)</code>
 	 * 
 	 * @since 0.2
 	 */
 	public static boolean isEmpty(Collection<?> collection) {
-		
-		return  !hasElement(collection);
+
+		return !hasElement(collection);
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se a <code>Collection</code> passada por parâmetro <strong>não</strong> é <code>null</code> 
-	 * e possui elementos. 
+	 * Verifica se a <code>Collection</code> passada por parâmetro
+	 * <strong>não</strong> é <code>null</code> e possui elementos.
 	 * </p>
 	 * 
-	 * @param collection - Instância de <code>Collection</code> analisada.
+	 * @param collection
+	 *            - Instância de <code>Collection</code> analisada.
 	 * @return <code>hasElement(collection)</code>
 	 * 
 	 * @see #isEmpty(Collection)
@@ -271,33 +301,35 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static boolean isNotEmpty(Collection<?> collection) {
-		
-		return  hasElement(collection);
+
+		return hasElement(collection);
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o <code>Map</code> passado por parâmetro é <code>null</code> ou 
-	 * <strong>não</strong> possui elementos. 
+	 * Verifica se o <code>Map</code> passado por parâmetro é <code>null</code>
+	 * ou <strong>não</strong> possui elementos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Map</code> analisada.
+	 * @param map
+	 *            - Instância de <code>Map</code> analisada.
 	 * @return <code>!hasElement(map)</code>
 	 * 
 	 * @since 0.2
 	 */
 	public static boolean isEmpty(Map<?, ?> map) {
-		
-		return  !hasElement(map);
+
+		return !hasElement(map);
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o <code>Map</code> passado por parâmetro <strong>não</strong> é <code>null</code> 
-	 * e possui elementos. 
+	 * Verifica se o <code>Map</code> passado por parâmetro <strong>não</strong>
+	 * é <code>null</code> e possui elementos.
 	 * </p>
 	 * 
-	 * @param map - Instância de <code>Map</code> analisada.
+	 * @param map
+	 *            - Instância de <code>Map</code> analisada.
 	 * @return <code>hasElement(map)</code>
 	 * 
 	 * @see #isEmpty(Map)
@@ -305,10 +337,10 @@ public final class Collections {
 	 * @since 0.2
 	 */
 	public static boolean isNotEmpty(Map<?, ?> map) {
-		
+
 		return hasElement(map);
 	}
-	
+
 	/**
 	 * <p>
 	 * Indica se uma dada coleção tem elementos sem gerar NPE.
@@ -318,7 +350,8 @@ public final class Collections {
 	 * <code>(c != null && !c.isEmpty())</code>.
 	 * </p>
 	 * 
-	 * @param c - A coleção a ser testada.
+	 * @param c
+	 *            - A coleção a ser testada.
 	 * 
 	 * @return (c != null && !c.isEmpty())
 	 * 
@@ -338,7 +371,8 @@ public final class Collections {
 	 * <code>(m != null && !m.isEmpty())</code>.
 	 * </p>
 	 * 
-	 * @param m - O mapa a ser testado.
+	 * @param m
+	 *            - O mapa a ser testado.
 	 * 
 	 * @return (m != null && !m.isEmpty())
 	 * 
@@ -348,7 +382,7 @@ public final class Collections {
 
 		return (m != null && !m.isEmpty());
 	}
-	
+
 	/**
 	 * <p>
 	 * Retorna a quantidade de elementos de uma dada coleção sem gerar NPE.
@@ -386,65 +420,87 @@ public final class Collections {
 
 		return (m != null ? m.size() : 0);
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o coleção passado por parâmetro <strong>não</strong> é <code>null</code> ou se é vazio.
+	 * Verifica se o coleção passado por parâmetro <strong>não</strong> é
+	 * <code>null</code> ou se é vazio.
 	 * </p>
 	 * <p>
-	 * Caso o objeto seja <code>null</code>, lança <code>NullPointerException</code> com a mensagem informada no
-	 * parâmetro <code>messageNullPointer</code> (primeiro parâmetro String). Caso o objeto não seja 
-	 * <code>null</code> e não possua elementos, lança <code>IllegalArgumentException</code> com a mensagem 
-	 * informada no parâmetro <code>messageIllegalArgument</code> (segundo parâmetro String).
+	 * Caso o objeto seja <code>null</code>, lança
+	 * <code>NullPointerException</code> com a mensagem informada no parâmetro
+	 * <code>messageNullPointer</code> (primeiro parâmetro String). Caso o
+	 * objeto não seja <code>null</code> e não possua elementos, lança
+	 * <code>IllegalArgumentException</code> com a mensagem informada no
+	 * parâmetro <code>messageIllegalArgument</code> (segundo parâmetro String).
 	 * </p>
 	 * 
-	 * @param collection - Objeto analisado.
-	 * @param messageNullPointer - Mensagem utiliada na exceção <code>IllegalArgumentException</code>.
-	 * @param messageIllegalArgument - Mensagem utiliada na exceção <code>IllegalArgumentException</code>.
+	 * @param collection
+	 *            - Objeto analisado.
+	 * @param IllegalArgumentException
+	 *            - Mensagem utiliada na exceção
+	 *            <code>IllegalArgumentException</code>.
+	 * @param messageIllegalArgument
+	 *            - Mensagem utiliada na exceção
+	 *            <code>IllegalArgumentException</code>.
+	 * 
+	 * @thows IllegalArgumentException
 	 * 
 	 * @since 0.2
 	 */
-	public static void checkNotEmpty(Collection<?> collection, String messageNullPointer, String messageIllegalArgument) {
-		
+	public static void checkNotEmpty(Collection<?> collection,
+			String messageNullPointer, String messageIllegalArgument) {
+
 		if (collection == null) {
-			
+
 			throw new IllegalArgumentException(messageNullPointer);
 		}
-		
+
 		if (collection.isEmpty()) {
-			
+
 			throw new IllegalArgumentException(messageIllegalArgument);
-		} 
+		}
 	}
-	
+
 	/**
 	 * <p>
-	 * Verifica se o mapa passado por parâmetro <strong>não</strong> é <code>null</code> ou se é vazio.
+	 * Verifica se o mapa passado por parâmetro <strong>não</strong> é
+	 * <code>null</code> ou se é vazio.
 	 * </p>
 	 * <p>
-	 * Caso o objeto seja <code>null</code>, lança <code>NullPointerException</code> com a mensagem informada no
-	 * parâmetro <code>messageNullPointer</code> (primeiro parâmetro String). Caso o objeto não seja 
-	 * <code>null</code> e não possua elementos, lança <code>IllegalArgumentException</code> com a mensagem 
-	 * informada no parâmetro <code>messageIllegalArgument</code> (segundo parâmetro String).
+	 * Caso o objeto seja <code>null</code>, lança
+	 * <code>NullPointerException</code> com a mensagem informada no parâmetro
+	 * <code>messageNullPointer</code> (primeiro parâmetro String). Caso o
+	 * objeto não seja <code>null</code> e não possua elementos, lança
+	 * <code>IllegalArgumentException</code> com a mensagem informada no
+	 * parâmetro <code>messageIllegalArgument</code> (segundo parâmetro String).
 	 * </p>
 	 * 
-	 * @param map - Objeto analisado.
-	 * @param messageNullPointer - Mensagem utiliada na exceção <code>IllegalArgumentException</code>.
-	 * @param messageIllegalArgument - Mensagem utiliada na exceção <code>IllegalArgumentException</code>.
+	 * @param map
+	 *            - Objeto analisado.
+	 * @param messageNullPointer
+	 *            - Mensagem utiliada na exceção
+	 *            <code>IllegalArgumentException</code>.
+	 * @param messageIllegalArgument
+	 *            - Mensagem utiliada na exceção
+	 *            <code>IllegalArgumentException</code>.
+	 * 
+	 * @thows IllegalArgumentException
 	 * 
 	 * @since 0.2
 	 */
-	public static void checkNotEmpty(Map<?,?> map, String messageNullPointer, String messageIllegalArgument) {
-		
+	public static void checkNotEmpty(Map<?, ?> map, String messageNullPointer,
+			String messageIllegalArgument) {
+
 		if (map == null) {
-			
+
 			throw new IllegalArgumentException(messageNullPointer);
 		}
-		
+
 		if (map.isEmpty()) {
-			
+
 			throw new IllegalArgumentException(messageIllegalArgument);
-		} 
+		}
 	}
-	
+
 }
